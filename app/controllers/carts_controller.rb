b1d6591277c38c548487b3b9f50b1c5024dba392
Ashutosh <rai137506@gmail.com>
2021-10-25 22:45:44 +0530

    data

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,8 +1,12 @@
 class CartsController < ApplicationController
   def create
    # @cart= Vegetable.find(params[:vegetable_id])
-   @cart= Cart.new(vegetable_id: params[:vegetable_id])
-   @cart.save
+   @cart= Cart.new(vegetable_id: params[:vegetable_id],
+                   user_id: params[:user_id],
+                   vegetable_name: params[:vegetable_name])
+   if @cart.save
+   @cartdata= Cart.where(user_id: params[:user_id])
+   end
    #@cart= Cart.vegetables(params[:vegetable_id])
   end
   
